dc9741740185ccec5b1e776a8810cf892f6d1f44
Sigfrid Stjärnholm <sigfrid@stjarnholm.com>
2024-01-07 19:32:55 +0100

    Make category in grid list card conditional, temporary image

--- components/EntryGridList.tsx
+++ components/EntryGridList.tsx
@@ -34,13 +34,16 @@ const EntryGridList = (props: Props) => {
             <div className="flex-1 truncate">
               <div className="flex items-center space-x-3">
                 <h3 className="truncate text-sm font-medium text-gray-900">{entry.name}</h3>
-                <span className="inline-flex flex-shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-                  {entry.category}
-                </span>
+                {!!entry.category && (
+                  <span className="inline-flex flex-shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                    {entry.category}
+                  </span>
+                )}
               </div>
               <p className="mt-1 truncate text-sm text-gray-500">{getEntrySubtitle(entry)}</p>
             </div>
-            <img className="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300" src={entry.imageSmall} alt="" />
+            {/* <img className="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300" src={entry.imageSmall} alt="" /> */}
+            <img className="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300" src="img/2447172.png" alt="" />
           </div>
           <div>
             <div className="-mt-px flex divide-x divide-gray-200">
