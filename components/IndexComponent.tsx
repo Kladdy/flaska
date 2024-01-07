f37a900e36f9e8fe4b7f1ef1e36f352de7cab02b
Sigfrid Stjärnholm <sigfrid@stjarnholm.com>
2024-01-07 22:36:14 +0100

    Add onClick event to "Lägg till flaska" button

--- components/IndexComponent.tsx
+++ components/IndexComponent.tsx
@@ -47,6 +47,7 @@ const IndexComponent = (props: Props) => {
           <button
             type="button"
             className="inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={() => setShowEntryModal(true)}
           >
             <PlusIcon className="-ml-0.5 h-5 w-5" aria-hidden="true" />
             Lägg till flaska
