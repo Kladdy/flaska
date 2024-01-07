9ea889ee7c5f781a0fcee79d53f44f0a3b9457c3
Sigfrid Stjärnholm <sigfrid@stjarnholm.com>
2024-01-07 21:50:16 +0100

    Add Link component and update UI layout

--- components/IndexComponent.tsx
+++ components/IndexComponent.tsx
@@ -6,6 +6,7 @@ import { Dialog, Transition } from "@headlessui/react";
 import CheckIcon from "@heroicons/react/16/solid/CheckIcon";
 import { PhotoIcon } from "@heroicons/react/24/solid";
 import EntryModal from "./EntryModal";
+import Link from "next/link";
 
 interface Props {
   user: UserProfile;
@@ -33,30 +34,37 @@ const IndexComponent = (props: Props) => {
   };
 
   return (
-    <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-      <div className="flex flex-col items-center my-10">
-        <h1 className="text-6xl font-bold bg-gradient-to-r from-green-400 to-blue-500 bg-clip-text text-transparent">
-          flaska
-        </h1>
-        <h3 className="flex flex-col items-center text-l font-semibold mt-5">
-          <p className="">Välkommen, {props.user.name}!</p>
-          <a className="hover:underline" href="/api/auth/logout">Logga ut</a>
-        </h3>
-      </div>
+    <>
+      <EntryModal show={showEntryModal} showModalAction={showModalHandler} handleSavedEntry={handleSavedEntry} user={props.user} />
+      <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div className="flex flex-col items-center my-10">
+          <Link
+            href="/"
+          >
+            <h1 className="text-6xl font-bold bg-gradient-to-r from-green-400 to-blue-500 bg-clip-text text-transparent">
+              flaska
+            </h1>
+          </Link>
+          
+          <h3 className="flex flex-col items-center text-l font-semibold mt-5">
+            <p className="">Välkommen, {props.user.name}!</p>
+            <a className="hover:underline" href="/api/auth/logout">Logga ut</a>
+          </h3>
+        </div>
 
-      <div className="flex flex-col items-center my-10">
-        <button
-          type="button"
-          className="rounded bg-indigo-500 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-          onClick={() => setShowEntryModal(true)}
-        >
-          Lägg till flaska
-        </button>
-        <EntryModal show={showEntryModal} showModalAction={showModalHandler} handleSavedEntry={handleSavedEntry} user={props.user} />
-      </div>
+        <div className="flex flex-col items-center my-10">
+          <button
+            type="button"
+            className="rounded bg-indigo-500 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+            onClick={() => setShowEntryModal(true)}
+          >
+            Lägg till flaska
+          </button>
+        </div>
 
-      <EntryGridList entrys={entrys} />
-    </div>
+        <EntryGridList entrys={entrys} />
+      </div>
+    </>
   );
 };
 
