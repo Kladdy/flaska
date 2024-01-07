eb3262bd96194e6ef812ba87137026c30d2dae15
Sigfrid Stjärnholm <sigfrid@stjarnholm.com>
2024-01-07 22:35:46 +0100

    Make button better

--- components/IndexComponent.tsx
+++ components/IndexComponent.tsx
@@ -8,6 +8,7 @@ import { PhotoIcon } from "@heroicons/react/24/solid";
 import EntryModal from "./EntryModal";
 import Link from "next/link";
 import TitleComponent from "./TitleComponent";
+import { PlusIcon } from "@heroicons/react/20/solid";
 
 interface Props {
   user: UserProfile;
@@ -45,9 +46,9 @@ const IndexComponent = (props: Props) => {
         <div className="flex flex-col items-center my-10">
           <button
             type="button"
-            className="rounded bg-indigo-500 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-            onClick={() => setShowEntryModal(true)}
+            className="inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
+            <PlusIcon className="-ml-0.5 h-5 w-5" aria-hidden="true" />
             Lägg till flaska
           </button>
         </div>
