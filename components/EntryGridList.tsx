ceca8a24ed6f255a57910d8869dc8a17d3d974a6
Sigfrid Stjärnholm <sigfrid@stjarnholm.com>
2024-01-20 21:15:06 +0100

    Fix some boarder and hover colors in grid list darkmode

--- components/EntryGridList.tsx
+++ components/EntryGridList.tsx
@@ -61,10 +61,10 @@ const EntryGridList = (props: Props) => {
   return (
     <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
       {props.entrys.map((entry) => (
-        <li key={entry._id!.toString()} className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow border-2 dark:bg-slate-800 dark:divide-gray-400 dark:border-gray-600">
+        <li key={entry._id!.toString()} className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow border-2 dark:bg-slate-800 dark:divide-gray-500 dark:border-gray-600">
           <Link 
             href={`/entry?id=${entry._id}`}
-            className="hover:bg-gray-200 hover:rounded-t-lg flex w-full items-center justify-between space-x-6 p-6 dark:hover:bg-gray-600"
+            className="hover:bg-gray-200 hover:rounded-t-lg flex w-full items-center justify-between space-x-6 p-6 dark:hover:bg-gray-700"
           >
             <div className="flex-1 truncate">
               <div className="flex items-center space-x-3">
@@ -82,12 +82,12 @@ const EntryGridList = (props: Props) => {
             <img className="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300" src={getEntryCategory(entry).icon} alt="" />
           </Link>
           <div>
-            <div className="-mt-px flex divide-x divide-gray-200">
+            <div className="-mt-px flex divide-x divide-gray-200 dark:divide-gray-500">
               {!!getSystembolagetLink(entry) && (
                 <div className="flex w-0 flex-1">
                   <a
                     href={getSystembolagetLink(entry)}
-                    className="hover:bg-gray-200 relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900 dark:text-gray-100 dark:hover:bg-gray-600"
+                    className="hover:bg-gray-200 relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900 dark:text-gray-100 dark:hover:bg-gray-700"
                   >
                     <BuildingStorefrontIcon className="h-5 w-5 text-gray-400 dark:text-gray-300" aria-hidden="true" />
                     Systembolaget
@@ -98,7 +98,7 @@ const EntryGridList = (props: Props) => {
                 <div className="-ml-px flex w-0 flex-1">
                   <a
                     href={getVivinoLink(entry)}
-                    className="hover:bg-gray-200 relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900 dark:text-gray-100 dark:hover:bg-gray-600"
+                    className="hover:bg-gray-200 relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900 dark:text-gray-100 dark:hover:bg-gray-700"
                   >
                     <StarIcon className="h-5 w-5 text-gray-400 dark:text-gray-300" aria-hidden="true" />
                     Vivino
@@ -109,7 +109,7 @@ const EntryGridList = (props: Props) => {
                 <div className="flex w-0 flex-1">
                   <Link 
                     href={`/entry?id=${entry._id}`}
-                    className="hover:bg-gray-200 relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-b-lg border border-transparent py-4 text-sm font-semibold text-gray-900  dark:hover:bg-gray-600"
+                    className="hover:bg-gray-200 relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-b-lg border border-transparent py-4 text-sm font-semibold text-gray-900  dark:hover:bg-gray-700"
                   >
                     <ArrowRightIcon className="h-5 w-5 text-gray-400 dark:text-gray-300" aria-hidden="true" />
                   </Link>
